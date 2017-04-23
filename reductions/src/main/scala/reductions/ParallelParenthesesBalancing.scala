420c9d07f7ec5adfed9fcdcc2d5955335c597ac4
Jay Zeng <jay.zeng@hotmail.com>
2017-04-23 23:26:38 +0800

    adding week 2 pararell programming assignment

---
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -0,0 +1,71 @@
+package reductions
+
+import org.scalameter._
+
+object ParallelParenthesesBalancingRunner {
+
+  @volatile var seqResult = false
+
+  @volatile var parResult = false
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 40,
+    Key.exec.maxWarmupRuns -> 80,
+    Key.exec.benchRuns -> 120,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val length = 100000000
+    val chars = new Array[Char](length)
+    val threshold = 10000
+    val seqtime = standardConfig measure {
+      seqResult = ParallelParenthesesBalancing.balance(chars)
+    }
+    println(s"sequential result = $seqResult")
+    println(s"sequential balancing time: $seqtime ms")
+
+    val fjtime = standardConfig measure {
+      parResult = ParallelParenthesesBalancing.parBalance(chars, threshold)
+    }
+    println(s"parallel result = $parResult")
+    println(s"parallel balancing time: $fjtime ms")
+    println(s"speedup: ${seqtime / fjtime}")
+  }
+}
+
+object ParallelParenthesesBalancing {
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def balance(chars: Array[Char]): Boolean = {
+
+    def balanced(xs: Array[Char], open: Int): Boolean = {
+      if(xs.isEmpty) open == 0
+      else if(xs.head == '(') balanced(xs.tail, open + 1)
+      else if(xs.head == ')') open > 0 && balanced(xs.tail, open -1)
+      else balanced(xs.tail, open)
+    }
+    balanced(chars, 0)
+  }
+
+  /** Returns `true` iff the parentheses in the input `chars` are balanced.
+   */
+  def parBalance(chars: Array[Char], threshold: Int): Boolean = {
+
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
+        
+    }
+
+    def reduce(from: Int, until: Int) /*:???*/ = {
+      ???
+    }
+
+    reduce(0, chars.length) == ???
+  }
+
+  // For those who want more:
+  // Prove that your reduction operator is associative!
+
+
+}
