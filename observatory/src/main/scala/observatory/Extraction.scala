30906d5212b562a2e8d2bb4f52481e2f0c6df72b
Jay Zeng <jay.zeng@hotmail.com>
2017-06-14 23:22:08 +0800

    adding a method withut using spark sql

--- observatory/src/main/scala/observatory/Extraction.scala
+++ observatory/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,17 @@
 package observatory
 
+import java.io.{BufferedReader, InputStreamReader}
 import java.time.LocalDate
+
+import scala.io.Source
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  //@transient lazy val conf: SparkConf = new SparkConf().setMaster("local").setAppName("StackOverflow")
+  //@transient lazy val sc: SparkContext = new SparkContext(conf)
 
   /**
     * @param year             Year number
@@ -14,7 +20,19 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    ???
+
+    val stations = Source.fromFile(stationsFile).getLines().map(line => line.split(",")).filter( s => s(2) != "" && s(3) != "")
+    val stationPairs = stations.map(line => (Key(line(0), line(1)), Location(line(2).toDouble, line(3).toDouble))).toMap
+    val temperatures = Source.fromFile(temperaturesFile).getLines().map(line => line.split(","));
+    val temperaturePair = temperatures.map(line => (Key(line(0), line(1)), LocalDate.of(year, line(2).toInt, line(3).toInt), line(4).toDouble))
+    temperaturePair.map(temp => {
+      val location = stationPairs.get(temp._1)
+      (temp._2, location, (temp._3 - 32)* (5 / 9))
+    })
+
+
+
+
   }
 
   /**
@@ -24,5 +42,6 @@ object Extraction {
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Double)]): Iterable[(Location, Double)] = {
     ???
   }
-
 }
+
+case class Station(stnId: Int, wbanId:)
--- observatory/src/main/scala/observatory/models.scala
+++ observatory/src/main/scala/observatory/models.scala
@@ -4,3 +4,5 @@ case class Location(lat: Double, lon: Double)
 
 case class Color(red: Int, green: Int, blue: Int)
 
+case class Key(stnId: String, wbanId: String)
+
