4b1d13b181064f096292f0522df4511a830c3c3d
Jay Zeng <jay.zeng@hotmail.com>
2017-06-16 23:44:35 +0800

    starts the visulization

--- observatory/src/main/scala/observatory/Visualization.scala
+++ observatory/src/main/scala/observatory/Visualization.scala
@@ -1,11 +1,15 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.Image
+import scala.math._
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
+
+  val radius = 6371
+  val power = 2
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
@@ -13,7 +17,17 @@ object Visualization {
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+
+    val tempDistancePairs = temperatures.map(p => (p._2, greatCyclePhi(p._1, location) * radius)).toMap
+      //.filter(pair => pair._1 < 1)
+    val weights = tempDistancePairs.mapValues( 1 / pow(_, power))
+
+    weights.map(p => p._1 * p._2).sum / weights.values.sum
+
+  }
+
+  def greatCyclePhi(p1 : Location, p2 : Location ) : Double  = {
+    acos(sin(p1.lat) * sin(p2.lat) + cos(p1.lat) * cos(p2.lat) * cos(abs(p1.lon - p2.lon)))
   }
 
   /**
