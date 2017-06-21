31722b021c7a5da8a24253deb9627db1f3dfd708
Jay Zeng <jay.zeng@hotmail.com>
2017-06-21 20:31:07 +0800

    adding progress

--- observatory/src/main/scala/observatory/Interaction.scala
+++ observatory/src/main/scala/observatory/Interaction.scala
@@ -1,6 +1,7 @@
 package observatory
 
-import com.sksamuel.scrimage.Image
+import com.sksamuel.scrimage.{Image, Pixel}
+import observatory.Visualization.{convert, interpolateColor}
 
 import scala.math._
 
@@ -28,6 +29,18 @@ object Interaction {
     * @return A 256×256 image showing the contents of the tile defined by `x`, `y` and `zooms`
     */
   def tile(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)], zoom: Int, x: Int, y: Int): Image = {
+
+    val colorCoordinatePairs  = temperatures.map(p => (convert(p._1), interpolateColor(colors, p._2)))
+    val pixels = colorCoordinatePairs.map(p => (p._1._1 + p._1._1 * p._1._2, Pixel(p._2.red, p._2.green, p._2.blue, 127)))
+
+    val locations = for { a <- x to x + 255
+                               b <- y to y + 255
+                          } yield tileLocation(zoom, a, b)
+
+
+
+    pixels.foreach(p => locations.updated(p._1, p._2))
+//    Image(256, 256, pixelArray)
     ???
   }
 
